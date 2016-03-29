db1b05f6518971ff4e74cc3cf96c65be9c4c4ff5
Alexander Grebenschikov <me@package.su>
2016-03-29 21:30:55 +0300

    remove lazy load

--- Photos/AssetImageView.m
+++ Photos/AssetImageView.m
@@ -11,7 +11,6 @@
 @interface AssetImageView ()
 
 @property (nonatomic) PHImageRequestID request;
-@property (nonatomic) NSInteger pointer;
 
 @end
 
@@ -27,42 +26,33 @@
 
 - (void)loadFromAsset:(PHAsset *)asset withSize:(CGSize)size square:(BOOL)square {
     [[PHImageManager defaultManager] cancelImageRequest:self.request];
-    NSInteger pointer = ++self.pointer;
     
     if (!asset) {
         self.image = [UIImage imageNamed:@"EmptyAlbum"];
         return;
     }
     
-    self.image = nil;
     __weak AssetImageView *weakSelf = self;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(100 * NSEC_PER_MSEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-        if (!weakSelf || pointer != weakSelf.pointer) {
+    
+    CGFloat scale = [UIScreen mainScreen].scale;
+    CGSize realSize = CGSizeApplyAffineTransform(size, CGAffineTransformMakeScale(scale, scale));
+    
+    PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+    if (square) {
+        options.resizeMode = PHImageRequestOptionsResizeModeExact;
+        CGFloat minSide = MIN(asset.pixelWidth, asset.pixelHeight);
+        CGFloat width = minSide / asset.pixelWidth;
+        CGFloat height = minSide / asset.pixelHeight;
+        options.normalizedCropRect = CGRectMake((1 - width) / 2, (1 - height) / 2, width, height);
+    }
+    
+    self.request = [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:realSize contentMode:PHImageContentModeAspectFit options:options resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
+        if (!result || !weakSelf) {
             return;
         }
         
-        CGFloat scale = [UIScreen mainScreen].scale;
-        CGSize realSize = CGSizeApplyAffineTransform(size, CGAffineTransformMakeScale(scale, scale));
-        
-        PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
-        if (square) {
-            options.resizeMode = PHImageRequestOptionsResizeModeExact;
-            CGFloat minSide = MIN(asset.pixelWidth, asset.pixelHeight);
-            CGFloat width = minSide / asset.pixelWidth;
-            CGFloat height = minSide / asset.pixelHeight;
-            options.normalizedCropRect = CGRectMake((1 - width) / 2, (1 - height) / 2, width, height);
-        }
-        
-        self.request = [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:realSize contentMode:PHImageContentModeAspectFit options:options resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
-            if (!result || !weakSelf) {
-                return;
-            }
-            
-            dispatch_async(dispatch_get_main_queue(), ^{
-                weakSelf.image = result;
-            });
-        }];
-    });
+        weakSelf.image = result;
+    }];
 }
 
 @end
